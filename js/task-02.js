668b5e2317b2b6bec36a4c52761bf596b96dc0d3
Yelena Leri <yelenaleri@gmail.com>
2022-09-08 22:53:46 +0300

    add 2 ex

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(option => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = `${option}`;
+  itemEl.classList.add('item');
+  return itemEl;
+});
+console.log(elements);
+
+listEl.append(...elements);
