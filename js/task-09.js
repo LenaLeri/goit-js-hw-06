b5130b611915f16f76d6c8c747de7395e33b945e
Yelena Leri <yelenaleri@gmail.com>
2022-09-11 23:37:52 +0300

    add 9 ex

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorCode = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const bodyColor = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', handleChangeClick);
+
+function handleChangeClick() {
+  const color = getRandomHexColor();
+  colorCode.textContent = color;
+  bodyColor.style.backgroundColor = color;
+}
