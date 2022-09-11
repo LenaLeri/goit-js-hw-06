3bdc51d2c9f5e831b8c4877bdfd26fd8e2b09755
Yelena Leri <yelenaleri@gmail.com>
2022-09-11 21:19:04 +0300

    add 7 ex

---
+++ js/task-07.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+textEl.style.fontSize = `${inputEl.value}px`;
+
+inputEl.addEventListener('input', handleFontSizeChange);
+
+function handleFontSizeChange(event) {
+  const fontSize = event.target.value;
+  textEl.style.fontSize = `${fontSize}px`;
+}
